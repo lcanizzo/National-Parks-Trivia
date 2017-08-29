7eda142a0286a8d6b487fa6330ce50a7b8f7fd6b
unknown <luca.canizzo@gmail.com>
2017-08-28 21:04:59 -0400

    added file structure, pseudocode, and outline of index.html file

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+// First Page:
+
+// Welcome
+// Mention the timer
+// Good Luck
+// START!
+//     -When they click the start button:  
+//         -the content will display none (disappear)
+//         -the first question will appear
+//         -the first timer will start running
+//         -the options for the first question will appear
+
+// First Question / Question pages:
+
+// The timer will start when the page opens
+//     -the timer will display on the page as: 10sec left, 9sec left... etc.
+//     -when the timer reaches 0:
+//         -the unanswered score will increase by 1
+//         -the content of the page will disappear
+//         -the content of the next page will display
+// The question will display
+//     -options will function as buttons
+//     -if the correct function is clicked:
+//         -correct score will increase by 1
+//         -the timer will be cancelled
+//         -the content of the page will disappear
+//         -the content of the next page will display 
+//     -if any other button is clicked:
+//         -incorrect score will increase by 1
+//         -the timer will be cancelled
+//         -the content of the page will disappear
+//         -the content of the next page will display 
+
+// Correct / Incorrect Page:
+
+// If the previous page answer was correct:
+//     -content will read, "Correct! _____ " 
+//     -a gif will display
+// If the previous page answer was inccorect:
+//     -content will read, "Nope! _______ was correct"
+//     -a gif will display
+// After the gif has finished (set time):
+//     -the content of the page will disappear
+//     -the content of the next page will display 
+// OR (maybe add later) onclick too:
+//     -the content of the page will disappear
+//     -the content of the next QUESTION page will display 
+//     -the next timer will start and update on the screen
+
+// Final Tally Page:
+
+// The tally of correct, unanswered, and incorrect answers will display
+// Given different score ranges, different html content will display
+// A reset button will display:
+//     -if the reset button is clicked:
+//          -the content of the page will disappear
+//          -the content of the FIRST QUESTION page will display (serves the same purpose as the welcome page)
+    
