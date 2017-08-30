2c984d2dc4e0ebb3c84a114ff62b6f3e20fc451a
unknown <luca.canizzo@gmail.com>
2017-08-29 23:01:34 -0400

    Added .js logic and fuller html structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,82 @@
-// First Page:
+// Commented out sections tabbed over have been addressed
+// Commented out sections adjacent to code mark problem areas
 
-// Welcome
-// Mention the timer
-// Good Luck
-// START!
-//     -When they click the start button:  
-//         -the content will display none (disappear)
-//         -the first question will appear
-//         -the first timer will start running
-//         -the options for the first question will appear
+$(document).ready(function(){
 
-// First Question / Question pages:
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
 
-// The timer will start when the page opens
-//     -the timer will display on the page as: 10sec left, 9sec left... etc.
-//     -when the timer reaches 0:
-//         -the unanswered score will increase by 1
-//         -the content of the page will disappear
-//         -the content of the next page will display
-// The question will display
-//     -options will function as buttons
-//     -if the correct function is clicked:
-//         -correct score will increase by 1
-//         -the timer will be cancelled
-//         -the content of the page will disappear
-//         -the content of the next page will display 
-//     -if any other button is clicked:
-//         -incorrect score will increase by 1
-//         -the timer will be cancelled
-//         -the content of the page will disappear
-//         -the content of the next page will display 
+    var currentQuestion = 0;
+    var $question = $('.question');
+    var totalQuestions = $('.question').length;
+    $question.hide();
 
-// Correct / Incorrect Page:
+    var currentAnswer = 0;
+    var $answer = $('.answer');
+    var totalAnswers = $('.answer').length;
+    $answer.hide();
+
+    $('#tally-page').hide();
+    $('.timer').hide();
+
+
+
+//      W E L C O M E  P A G E       //
+                                                //     -When they click the start button:  
+                                                //         -the content will display none (disappear)
+                                                //         -the first question will appear
+                                                //         -the options for the first question will appear
+                                                //         -the first timer will start running
+
+$('#start-button').on('click', function () {
+    $('#welcome-page').hide();
+    $($question.get(currentQuestion)).fadeIn();   
+    $('.timer').fadeIn(); 
+    startTimer();
+})
+
+//      Q U E S T I O N   P A G E S       //
+
+                                                // The timer will start when the page opens
+                                                //     -the timer will display on the page as: 10sec left, 9sec left... etc.
+                                                //     -when the timer reaches 0:
+                                                //         -the unanswered score will increase by 1
+                                                //         -the content of the page will disappear
+                                                //         -the content of the next page will display
+
+                                                // The question will display
+                                                //     -options will function as buttons
+                                                //     -if the correct function is clicked:
+                                                //         -correct score will increase by 1
+                                                //     -if any other button is clicked:
+                                                //         -incorrect score will increase by 1
+
+                                                // Enter button will:
+                                                //     -submit answer
+                                                //     -reset timer
+                                                //     -transition to next page
+
+                                                // the correct answer has a value of 1
+                                                // incorrect answers have a value of 0?
+                                                //      pull value???? (no spaces in jQuery selector)
+
+var chosenAnswer = 0;
+
+$('.enter').on('click', function(){
+// Tallies answer:
+    submitAnswer();
+// Resets Timer
+    resetTimer();
+// Goes to next answer:
+    nextQuestion();      //    Go to next answer, then to next question, time, etc.
+// Starts Timer
+    startTimer();
+// If last question:
+    tallyPage();
+})
+
+//      C O R R E C T   A N S W E R   P A G E S     //
 
 // If the previous page answer was correct:
 //     -content will read, "Correct! _____ " 
@@ -47,12 +92,149 @@
 //     -the content of the next QUESTION page will display 
 //     -the next timer will start and update on the screen
 
-// Final Tally Page:
 
-// The tally of correct, unanswered, and incorrect answers will display
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      F I N A L   T A L L Y   P A G E      //
+
+                                                    // The tally of correct, unanswered, and incorrect answers will display
 // Given different score ranges, different html content will display
-// A reset button will display:
-//     -if the reset button is clicked:
-//          -the content of the page will disappear
-//          -the content of the FIRST QUESTION page will display (serves the same purpose as the welcome page)
-    
+
+                                                    // A reset button will display:
+                                                    //     -if the reset button is clicked:
+                                                    //          -the content of the page will disappear
+                                                    //          -the content of the FIRST QUESTION page will display 
+                                                    //             -(serves the same purpose as the welcome page)
+                                                    //          -correct = 0
+                                                    //          -incorrect = 0
+                                                    //          -unanswered = 0
+ 
+$('.restart').on('click', function(){
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+    currentQuestion = 0;
+    $('#tally-page').hide();
+    $('#welcome-page').fadeIn();    
+    console.log('correct:', correct);
+    console.log('incorrect:', incorrect);
+    console.log('unanswered:', unanswered);
+})
+
+//      F U N C T I O N S       //
+
+function submitAnswer() {
+    chosenAnswer = $("input[name=radio]:checked").val();
+    console.log('chosenAnswer.val', chosenAnswer);
+
+    if (chosenAnswer == 1) {
+        correct ++;
+                                    //Display "That's Right" text option on next page //
+    } else if (chosenAnswer == 0) {
+        incorrect ++;
+                                    //Display "Nope" text option on next page //
+    } 
+    if (!$("input[name=radio]:checked").val()) {
+        unanswered ++;
+    }
+                                    //hitting enter without a selection logs previous pg. val
+    console.log('correct:', correct);
+    console.log('incorrect:', incorrect);
+    console.log('unanswered:', unanswered);
+}
+
+function timeUp () {
+    unanswered ++;
+    console.log('unanswered:', unanswered);
+}
+
+function nextQuestion () {
+    $($question.get(currentQuestion)).fadeOut();
+    currentQuestion = currentQuestion + 1;
+    if (currentQuestion == totalQuestions) {
+        console.log('Done with Quiz');
+    } else {
+        $($question.get(currentQuestion)).fadeIn();        
+    }
+}
+
+function nextQuestion () {
+    $($question.get(currentQuestion)).fadeOut();
+    currentQuestion = currentQuestion + 1;
+    if (currentQuestion == totalQuestions) {
+        console.log('Done with Quiz');
+    } else {
+        $($question.get(currentQuestion)).fadeIn();        
+    }
+}
+
+function nextAnswer () {
+    $($answer.get(currentAnswer)).fadeIn();
+}
+
+function tallyPage() {
+    if (currentQuestion == totalQuestions) {   //&& something else after last gif
+        $('#tally-page').fadeIn();             //remove calls of tallyPage where not needed
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        $("#unanswered").html(unanswered);
+        resetTimer();
+        $("#timer").hide();
+    }
+}
+
+//      T I M E R      //
+
+var time = 5;               //FIX FIX FIX FIX////FIX FIX FIX FIX////FIX FIX FIX FIX//
+
+function startTimer() {
+    intervalId = setInterval(decrement, 1000);
+    console.log('startTimer');
+    $('#timer').fadeIn();
+}
+
+function decrement() {
+    time--;
+    $('#timer').html(time);
+    if (time == 0) {
+        console.log('timesUp');
+        // Marks unanswered
+            timeUp();
+        // Resets Timer
+            resetTimer();
+        // Goes to next question:
+            nextQuestion();
+        // Starts Timer
+            startTimer();
+        // If last question:
+            tallyPage();
+        }
+}
+
+function resetTimer() {
+    clearInterval(intervalId);
+    time = 5;               //FIX FIX FIX FIX////FIX FIX FIX FIX////FIX FIX FIX FIX//
+    $('#timer').html(time);    
+}
+
+function stopTimer() {
+    console.log('stopTimer');
+    clearInterval(intervalId);
+}
+
+})
